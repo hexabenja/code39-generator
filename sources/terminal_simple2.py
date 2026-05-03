ddd1bb026d7c42ec6eef3b1c71a40ca7bec93a44
hexabenja <benjaminignacio066@gmail.com>
2026-05-03 13:08:46 -0400

    Se corrigió error de binario para Linux configurando tkinter del archivo de terminal_simple2 importando tkinter as tk como sólamente tkinter y luego de aquello se compiló el programa usando pyinstaller para linux

--- sources/terminal_simple2.py
+++ sources/terminal_simple2.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+import tkinter
 from tkinter import scrolledtext
 import barcode
 from barcode.writer import ImageWriter
@@ -14,7 +14,7 @@ class TerminalSimple:
         self.output.pack(expand=True, fill='both', padx=10, pady=10)
         
         # Campo de entrada
-        self.input_field = tk.Entry(root, bg='#252526', fg="#d4d4d4")
+        self.input_field = tkinter.Entry(root, bg='#252526', fg="#d4d4d4")
         self.input_field.pack(fill='x', padx=10, pady=10)
         self.input_field.bind('<Return>', self.procesar_entrada)
         
@@ -23,13 +23,13 @@ class TerminalSimple:
     
     def escribir(self, texto):
         self.output.config(state='normal')
-        self.output.insert(tk.END, texto)
-        self.output.see(tk.END)
+        self.output.insert(tkinter.END, texto)
+        self.output.see(tkinter.END)
         self.output.config(state='disabled', fg="#d4d4d4")
     
     def procesar_entrada(self, event=None):
         codigo = self.input_field.get().strip()
-        self.input_field.delete(0, tk.END)
+        self.input_field.delete(0, tkinter.END)
         
         if not codigo: return
         if codigo.lower() in ['salir', 'ya']:
@@ -42,17 +42,13 @@ class TerminalSimple:
     def generar_codigo(self, texto):
             code39 = barcode.get_barcode_class('code39')
             writer = ImageWriter()
-            options = {
-                "font_path": "arial.ttf", # Windows
-                "font_size": 10,
-                "text_distance": 5
-            }
+
             # Genera y guarda directamente
             nombre = code39(texto.upper(), writer=ImageWriter(), add_checksum=False).save(texto.upper())
             self.escribir(f"Archivo generado: {nombre}\n")
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    root = tkinter.Tk()
     TerminalSimple(root)
     root.mainloop()
